58a5622d28d31c2bd5662f47cc9a8dde13a7a66b
Alex Cordonnier <alexjcord@gmail.com>
2017-02-26 00:37:07 -0600

    Average the RSSI over two seconds

--- WarmerColder_Tag/main.cpp
+++ WarmerColder_Tag/main.cpp
@@ -2,11 +2,11 @@
 #include "ble/BLE.h"
 #include "color.h"
 
-#define TINY_BLE        1
+//#define TINY_BLE
 
 #define BAUD_RATE       115200 // baud
 
-#define BLINK_INTERVAL  600 // ms
+#define UPDATE_INTERVAL 500 // ms
 
 #define SCAN_INTERVAL   160 // units of 0.625ms
 #define SCAN_WINDOW     SCAN_INTERVAL // units of 0.625ms
@@ -39,10 +39,20 @@ PwmOut green_led(GREEN_PIN);
 PwmOut blue_led(BLUE_PIN);
 
 Ticker t;
+Timer clk;
 Serial serial(p9, p11);
 BLE& ble = BLE::Instance(BLE::DEFAULT_INSTANCE);
 
-float hue = 0.0; // degrees
+struct datapoint {
+    int timestamp;
+    int rssi;
+};
+
+// Circular buffer for calculating rolling average
+#define AVG_LAST_N_MILLISECONDS         2000
+#define MAX_RECEIVED_ADVERTISEMENTS     50
+struct datapoint receivedAdvertisements[MAX_RECEIVED_ADVERTISEMENTS];
+int recvAdvHeadIdx, recvAdvTailIdx;
 
 
 void setRGB(float r, float g, float b) {
@@ -57,8 +67,46 @@ void setHSV(float h, float s, float v) {
     setRGB(r, g, b);
 }
 
-void ledUpdateCallback(void) {
+void setHue(int rssi) {
+    float hue;
+    if (rssi > -80) {
+        hue = 0;
+    } else if (rssi > -90) {
+        hue = 335;
+    } else if (rssi > -100) {
+        hue = 260;
+    } else {
+        hue = 240;
+    }
     setHSV(hue, 1, 1);
+}
+
+void ledUpdateCallback(void) {
+    int sum = 0;
+    int total = 0;
+    int avg = -105;
+    int i = recvAdvTailIdx;
+    while (i != recvAdvHeadIdx) {
+        struct datapoint dp = receivedAdvertisements[i];
+        if ((clk.read_ms() - dp.timestamp) > AVG_LAST_N_MILLISECONDS) {
+            // Remove old data points
+            recvAdvTailIdx = (recvAdvTailIdx + 1) % MAX_RECEIVED_ADVERTISEMENTS;
+        } else {
+            sum += dp.rssi;
+            total++;
+        }
+        i = (i + 1) % MAX_RECEIVED_ADVERTISEMENTS;
+    }
+    if (total > 0) {
+        avg = sum / total;
+    } else {
+        printf("%d, %d, %d\n", recvAdvTailIdx, recvAdvHeadIdx, i);
+    }
+#ifdef TINY_BLE
+    printf("Avg RSSI: %d\n", avg);
+#endif
+    
+    setHue(avg);
     wait(0.1);
     setRGB(0, 0, 0);
 }
@@ -70,7 +118,12 @@ void advertisementReceivedCallback(const Gap::AdvertisementCallbackParams_t *par
 #ifdef TINY_BLE
         printf("%d\n", params->rssi);
 #endif
-        hue = ((params->rssi - RSSI_RED)*2 + 360) % 360;
+        receivedAdvertisements[recvAdvHeadIdx].timestamp = clk.read_ms();
+        receivedAdvertisements[recvAdvHeadIdx].rssi = params->rssi;
+        recvAdvHeadIdx = (recvAdvHeadIdx + 1) % MAX_RECEIVED_ADVERTISEMENTS;
+        if(recvAdvHeadIdx == recvAdvTailIdx) {
+            recvAdvTailIdx = (recvAdvTailIdx + 1) % MAX_RECEIVED_ADVERTISEMENTS;
+        }
     }
 }
 
@@ -107,8 +160,10 @@ void bleInitCallback(BLE::InitializationCompleteCallbackContext *params) {
 }
 
 int main(void) {
+    clk.start();
+    
     setRGB(0, 0, 0);
-    t.attach(ledUpdateCallback, BLINK_INTERVAL/1000.0);
+    t.attach(ledUpdateCallback, UPDATE_INTERVAL/1000.0);
 
 #ifdef TINY_BLE
     serial.baud(BAUD_RATE);
